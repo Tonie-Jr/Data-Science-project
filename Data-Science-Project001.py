d624d3f6caeda5f116f031ebe6b5f714c8bc8220
Tonnie <antonymuriithi505@gmail.com>
2024-09-03 02:14:08 +0300

    added a few functios

--- Data-Science-Project001.py
+++ Data-Science-Project001.py
@@ -1,6 +1,4 @@
 # Understanding between the mutable and immutable objects in python
-
-
 def get_largest_number(numbers,n):
     numbers.sort()
 
@@ -15,7 +13,7 @@ print(nums)
 #Formating the print function using the sep argument
 Age = 19
 name = "Waihuini"
-print("My name is", name, "and I'm ", Age, "years old", sep = "|" )# It is useful when one want to print items in a certain formatting.
+print("My name is", name, "and I'm ", Age, "years old", sep = "|" )# It is useful when one want to print items with a certain formatting.
 
 #Print items or different statements in the same line. we use the "end = "," " argument to do that
 name = "Antony"
@@ -24,8 +22,36 @@ print("I'm", name)
 
 help(name) #The help function is important to give the explanation of whatever in the braces.
 
- #The range function, it is used to iterate values in for loop or to generate a list of numbers.
+#The range function, it is used to iterate values in for loop or to generate a list of numbers.
 rng = range(10)
 print(rng)
 print(list(rng))
 
+#Map function the function allows us to apply the function to every single iterm in an iterable object.
+strings = ["My", "World", "Apple", "Pear"]
+
+length1 = map(len, strings)
+print(list(length1))
+
+#Example 2 of the map function Using the lambda function
+
+strings = ["My", "World", "Apple", "Pear"]
+
+length2 = map(lambda x: x +"s", strings)
+print(list(length2))
+
+#Example 3 of the map function Using my own function
+strings = ["My", "World", "Apple", "Pear"]
+
+def add_s(string):
+    return string + "s"
+length3 = map(add_s, strings)
+print(list(length3))
+
+#Filter function
+def longer_than_4(string):
+    return len(string) > 4
+
+strings = ["My", "World", "Apple", "Pear"]
+filtered = filter(longer_than_4, strings)
+print(list(filtered))
