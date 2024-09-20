c70edb631b93b1452e2afdfd5326690c08b6b1e7
Tonnie <antonymuriithi505@gmail.com>
2024-09-21 01:38:37 +0300

    Added function for factorial of example 2

--- Data-Science-Project001.py
+++ Data-Science-Project001.py
@@ -102,10 +102,21 @@ with open("test.txt", "r") as file:
     text = file.read()
     print(text)
 
-#factorail function
+#Simpel recursive Algorithms
+#example 1 factorial of a number
 def iterative_factorial(n):
     fact = 1
     for i in range(2, n+1):
         fact *= i
     return fact
 print(iterative_factorial(5))
+#Example 2 factorial of a number
+def recur_factorial(n):
+    if n == 1:
+        return 1
+    else:
+        temp = recur_factorial(n-1)
+        temp = temp * n
+    return temp
+print(recur_factorial(6))
+
