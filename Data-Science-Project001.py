f87a0d6a4d0e16f99a78ca687c6d7dd353dd04e8
Tonnie <antonymuriithi505@gmail.com>
2024-09-24 13:30:24 +0300

    added the iterative permutation function

--- Data-Science-Project001.py
+++ Data-Science-Project001.py
@@ -159,8 +159,21 @@ def permute(string, pocket=""):
             permute(together, letter + pocket)
 print(permute("ABC", ""))
 
-
-
-
-
-
+from math import factorial
+def permutation(str):
+    for p in range(factorial(len(str))):
+        print(''.join(str))
+        i = len(str) -1
+        while i > 0 and str[i - 1] > str[i]:
+            i -= 1
+        str[i:] = reversed(str[i:])
+        if i > 0:
+            q = i
+            while str[i - 1] > str[q]:
+                q+=1
+            tempp = str[i - 1]
+            str[i - 1] = str[q]
+            str[q] = tempp
+s = 'abc'
+s = list(s)
+print(permutation(s))
